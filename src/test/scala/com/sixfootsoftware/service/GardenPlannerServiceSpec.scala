f33083d6bf09b756cf8325a6eb3cb78ff43fb46f
Teqqles <david.long@racing-leagues.co.uk>
2017-07-30 21:16:49 +0100

    Added latin name to service test

--- src/test/scala/com/sixfootsoftware/service/GardenPlannerServiceSpec.scala
+++ src/test/scala/com/sixfootsoftware/service/GardenPlannerServiceSpec.scala
@@ -18,7 +18,7 @@ class GardenPlannerServiceSpec extends WordSpec with Matchers with ScalatestRout
     "Accept POST requests with vegetable facts" in {
 
       val carrot =
-        Vegetable(id = None, "Carrot", harvest = List(Feb, Mar), sowing = List(Feb), soilPreference = List(Acidic))
+        Vegetable(id = None, "Carrot", "Carrotus", harvest = List(Feb, Mar), sowing = List(Feb), soilPreference = List(Acidic))
       Post("/vegetable", carrot) ~> vegetable ~> check {
         status === OK
         responseAs[String] shouldBe carrot.toJson.toString
